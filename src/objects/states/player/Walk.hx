e1beb422cb1e75969ed6d6d14b83e2a17780a3f7
Eduardo Lopes <EduardoLopesDS@gmail.com>
2015-12-13 21:04:56 -0300

    Fix jump bug

--- src/objects/states/player/Walk.hx
+++ src/objects/states/player/Walk.hx
@@ -17,7 +17,7 @@ class Walk extends Move {
 
     super.update(dt);
 
-    if(Luxe.input.inputpressed('jump') && player.onGround == true){
+    if(Luxe.input.inputpressed('jump')){
 
       machine.set('jump');
 
