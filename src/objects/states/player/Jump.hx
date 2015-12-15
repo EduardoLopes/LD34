3656be51a54bd994116734a8ed13b6dc8ba7fd6d
Eduardo Lopes <EduardoLopesDS@gmail.com>
2015-12-15 19:14:27 -0300

    Fix crash due to animation event delayed

--- src/objects/states/player/Jump.hx
+++ src/objects/states/player/Jump.hx
@@ -6,6 +6,7 @@ class Jump extends Move {
 
   var jumps : Float = 1;
   var jumpForce : Float = -300;
+  var animation_end_event_id : String;
 
   public function new(Player:Player){
 
@@ -55,15 +56,20 @@ class Jump extends Move {
     player.onGround = false;
     player.body.velocity.y = jumpForce;
 
-    player.events.listen('animation.shoot.end', function(_){
+    animation_end_event_id = player.events.listen('animation.shoot.end', function(_){
 
-      Luxe.timer.schedule(0.1, function(){
         player.anim.animation = 'jump';
         player.anim.play();
-      });
 
     });
 
+  }
+
+  override function onLeave(){
+
+    super.onLeave();
+
+    player.events.unlisten(animation_end_event_id);
 
   }
 
--- src/objects/states/player/Walk.hx
+++ src/objects/states/player/Walk.hx
@@ -5,6 +5,8 @@ import objects.states.ObjectState;
 
 class Walk extends Move {
 
+  var animation_end_event_id : String;
+
   public function new(Player:Player){
 
     super(Player);
@@ -45,14 +47,20 @@ class Walk extends Move {
   override function onEnter(){
     super.onEnter();
 
-    player.events.listen('animation.shoot.end', function(_){
+    animation_end_event_id = player.events.listen('animation.shoot.end', function(_){
 
-      Luxe.timer.schedule(0.1, function(){
-        player.anim.animation = 'walk';
-        player.anim.play();
-      });
+      player.anim.animation = 'walk';
+      player.anim.play();
 
     });
+
+  }
+
+  override function onLeave(){
+
+    super.onLeave();
+
+    player.events.unlisten(animation_end_event_id);
 
   }
 
